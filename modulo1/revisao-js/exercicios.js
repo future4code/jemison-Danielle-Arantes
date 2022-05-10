d786706f0d1eef468b72057ea9166d406e38ef81
Danielle Silveira Arantes <artdanielle@hotmail.com>
2022-05-10 20:13:02 -0300

    Exercicio 15A

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -152,8 +152,12 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+
+    return consultas.sort((a,b) => a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0)
 }
+
+   // { nome: "João", dataDaConsulta: "01/10/2021" },
+  
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
