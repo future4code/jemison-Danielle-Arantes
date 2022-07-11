c2d07b97939aa41d77783e76c986a95b63851370
Danielle Silveira Arantes <artdanielle@hotmail.com>
2022-07-11 09:54:46 -0300

    finalizado

--- modulo3/projeto-whatslab/src/componentes/Main/main.js
+++ modulo3/projeto-whatslab/src/componentes/Main/main.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 
 //ESTILIZAÇÕES
 const PostMain = styled.div`
   background-color: white;
   width: 100%;
+  display: flex;
+  flex-direction: column;
   
 `
-
 const Form = styled.div`
   display: flex;
-  justify-content: center;
   width: 100%;
   gap: 0.5%;
   
@@ -21,25 +21,71 @@ const Form = styled.div`
     width: 65%;
   }
 `
+const Conversa = styled.div`
+    border: solid .04vw;
+    border-radius: 1vw;
+    box-shadow: 0.2vw grey;
+    margin: 0.5vw;
+    padding: 0.2vw;
+
+    .negrito{
+      font-weight: bold;
+    }
+`
+
 //FIM ESTILIZAÇÕES
 
 
-//FUNÇÃO PRINCIPAL
 function Main(){
 
+  //estados
+  const [inputMensagem, setInputMensagem] = useState ("")
+  const [inputNome, setInputNome] = useState ("")
+  const [listaMsg, setListaMsg] = useState ([])
+  //fim estados
+
+  //eventos
+  function handleInputNome (e){
+    setInputNome(e.target.value)
+  }
+  function handleInputMensagem (e){
+    setInputMensagem(e.target.value)
+  }
+
+  const addMsg = (e) => {
+    e.preventDefault()
+
+    setListaMsg ([{nome:inputNome, mensagem:inputMensagem}, ...listaMsg])
+    setInputMensagem("")
+    setInputNome("")
+  }
+  //fim eventos
+
+  //mapeando
+  const mensagemPost = listaMsg.map((msg, index) => {
+
+    return(
+      <Conversa key={index}>
+        <p><span className='negrito'>{msg.nome}:</span> {msg.mensagem}</p>
+      </Conversa>
+    )
+  })
 
   return(
     <PostMain>
+      {mensagemPost}
       <Form>
         <input className="menor"
           placeholder='Remetente'
-          value=""
+          value={inputNome}
+          onChange={handleInputNome}
         />
         <input className="maior"
           placeholder='Mensagem'
-          value=""
+          value = {inputMensagem}
+          onChange = {handleInputMensagem}
         />
-        <button>Enviar!</button>
+        <button onClick={addMsg}>Enviar!</button>
       </Form>
     </PostMain>
   )
