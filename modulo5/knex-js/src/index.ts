cf536c3e04ef3e26450f57c4ac2086c72cf465dd
Danielle Silveira Arantes <artdanielle@hotmail.com>
2022-11-22 19:25:47 -0300

    Knex js

--- modulo5/knex-js/src/index.ts
+++ modulo5/knex-js/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express'
 import cors from 'cors'
 import knex from "knex";
 import dotenv from "dotenv";
+import {connection} from "../database/connection";
 
 dotenv.config();
 
@@ -14,3 +15,20 @@ app.use(cors())
 app.listen(3000, () => {
     console.log('Server is running in http://localhost:3000');
 });
+
+const getActorById = async (id: string): Promise<any> => {
+    const result = await connection.raw(`
+      SELECT * FROM Actor WHERE id = '${id}'
+    `)
+  
+      return result[0][0]
+  }
+  
+  
+getActorById("001")
+    .then(result => {
+      console.log(result)
+    })
+    .catch(err => {
+      console.log(err)
+    });
